c050e5f701f7609002c87168ddb999b3e0d91ae6
Tushar Rajput <tusharo17rajput@gmail.com>
2025-10-01 09:20:43 +0530

    feat: implement message retrieval and update runLLM to accept messages

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import { runLLM } from './src/llm'
+import { getMessages } from './src/memory'
 
 const userMessage = process.argv[2]
 
@@ -8,5 +9,10 @@ if (!userMessage) {
   process.exit(1)
 }
 
-const response = await runLLM({ userMessage })
+const messages = await getMessages()
+
+const response = await runLLM({
+  messages: [...messages, { role: 'user', content: userMessage }],
+})
+
 console.log('Response:', response)
